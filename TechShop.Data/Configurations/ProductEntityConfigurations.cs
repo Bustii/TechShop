9f5a577c62167887367b6e35144b602cec4b3e59
Bustii@students.softuni.bg <Bustii@students.softuni.bg>
2023-08-01 15:43:12 +0300

    Changed backgroud screen.

--- TechShop.Data/Configurations/ProductEntityConfigurations.cs
+++ TechShop.Data/Configurations/ProductEntityConfigurations.cs
@@ -39,7 +39,7 @@
                 Description = "Intel Core i3-12100\r\nIntel UHD Graphics 730\r\nот 8GB до 32GB DDR4\r\nот 512GB SSD NVMe до 4TB (SSD и HDD)",
                 ImageUrl = "https://pisces.bbystatic.com/image2/BestBuy_US/images/products/6401/6401009_sd.jpg",
                 Price = 1300.00M,
-                CategoryId = 1
+                CategoryId = 3
                 //BuyerId = Guid.Parse("079F3635-F031-4C1E-9750-08DB740AA10F") //BuyerId
             };
             products.Add(product);
--- TechShop/Program.cs
+++ TechShop/Program.cs
@@ -37,7 +37,7 @@ namespace TechShop.Web
                 .AddEntityFrameworkStores<TechShopDbContext>();
 
             builder.Services.AddApplicationServices(typeof(IProductService));
-            builder.Services.AddTransient<IProductService, ProductService>();
+           // builder.Services.AddTransient<IProductService, ProductService>();
 
             builder.Services.ConfigureApplicationCookie(cfg =>
             {
